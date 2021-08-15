33ae21bf230de8e7c8647fd8224fce7b1db3d182
navishkauoc <navishka.randika@gmail.com>
2021-08-15 21:52:08 +0530

    Use of state in component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Greet from './components/Greet';
 import Hello from './components/Hello';
+import Message from './components/Message';
 import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
+      <Message />
+
+      {/* <Greet name="Bruce" heroName="Batman">
         <p>This is children props</p>
       </Greet>
       <Greet name="Clark" heroName="Superman">
@@ -15,7 +18,7 @@ function App() {
       <Greet name="Diana" heroName="Wonder Woman" />
       <Welcome name="Bruce" heroName="Batman" />
       <Welcome name="Clark" heroName="Superman" />
-      <Welcome name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Diana" heroName="Wonder Woman" /> */}
       {/*<Hello /> */}
     </div>
   );

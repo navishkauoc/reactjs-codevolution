de5d17a7455104b3adab3711c1ecd3df60baf241
navishkauoc <navishka.randika@gmail.com>
2021-08-17 00:59:58 +0530

    Use of setState() method

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,12 +3,13 @@ import Greet from './components/Greet';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Welcome from './components/Welcome';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message />
-
+      <Counter />
+      {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="Batman">
         <p>This is children props</p>
       </Greet>

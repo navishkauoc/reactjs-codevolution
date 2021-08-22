e0b5e3545d11b1f5d3876b4b85eea3634355062d
navishkauoc <navishka.randika@gmail.com>
2021-08-22 18:50:54 +0530

    Destructuring in the parameter

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,12 +8,12 @@ import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
       {/* <Message /> */}
-      {/* <Greet name="Bruce" heroName="Batman">
+      <Greet name="Bruce" heroName="Batman">
         <p>This is children props</p>
       </Greet>
-      <Greet name="Clark" heroName="Superman">
+      {/* <Greet name="Clark" heroName="Superman">
         <button>Action</button>
       </Greet>
       <Greet name="Diana" heroName="Wonder Woman" />
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -4,12 +4,11 @@ import React from 'react';
     return <h1>Hello Navishka</h1>
 } */
 
-const Greet = props => {
-    console.log(props);
+const Greet = ({name, heroName}) => {
     return (
         <div>
-            <h1>Hello {props.name} AKA {props.heroName}!</h1>
-            {props.children} 
+            <h1>Hello {name} AKA {heroName}!</h1>
+            {/* {props.children}  */}
         </div>   
     )
 }

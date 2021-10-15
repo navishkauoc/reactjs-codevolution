f74b38c1784754660ca530ff86e9c10e593d5c8c
navishkauoc <navishka.randika@gmail.com>
2021-10-15 23:50:07 +0530

    Child component added

---
+++ hello-world/src/components/ChildComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChildComponent() {
+    return (
+        <div>
+            <button>Greet Parernt</button>
+        </div>
+    )
+}
+
+export default ChildComponent
